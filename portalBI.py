815be8c6662b806c7fd8e752346f12e8b1ca3316
Juan Camilo Jaramillo Tascón <jcjt95@gmail.com>
2020-07-22 19:52:29 -0500

    Update portalBI.py

--- portalBI.py
+++ portalBI.py
@@ -2,7 +2,7 @@ import requests
 
 #http://portalbissrs.xm.com.co/dmnd/Paginas/Historicos/Historicos.aspx
 
-file = 'Demana_Comercial_SEM2_2020.xlsx'
+file = 'Demanda_Comercial_SEM2_2020.xlsx'
 link = 'http://portalbissrs.xm.com.co/dmnd/Histricos/Demanda_Comercial_Por_Comercializador_SEME2_2020.xlsx'
 
 print("")
